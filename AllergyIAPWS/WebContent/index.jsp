f20de22845dde74d104343f12d258665cdb1d834
Jaccao <jaccao@bitbucket.org>
2016-11-17 19:53:46 +0100

    last

--- AllergyIAPWS/WebContent/index.jsp
+++ AllergyIAPWS/WebContent/index.jsp
@@ -1,5 +1,9 @@
+<%@page import="java.sql.Statement"%>
+<%@page import="java.sql.Connection"%>
+<%@page import="javax.sql.DataSource"%>
+<%@page import="javax.naming.InitialContext"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +11,27 @@
 <title>Insert title here</title>
 </head>
 <body>
-Hello world
+	Hello world
+	<%
+	String strStatus = "testando";
+	try {
+		InitialContext cxt = new InitialContext();
+		if (cxt != null) {
+			DataSource ds = (DataSource) cxt.lookup("java:/PostgresXADS");
+			if (ds == null)
+				strStatus = "Error while obtaining the dataSource";
+			else {
+				Connection connection = ds.getConnection();
+				Statement stm = connection.createStatement();
+				stm.executeUpdate("insert into example (code, name) values('123','456')");
+				connection.close();
+				stm.close();
+			}
+		}
+	} catch (Exception e) {
+		strStatus = e.getMessage();
+	}
+	out.println(strStatus);
+%>
 </body>
 </html>
--- AllergyIAPWS/src/com/allergyiap/test/TestIndex.java
+++ AllergyIAPWS/src/com/allergyiap/test/TestIndex.java
@@ -1,11 +1,16 @@
 package com.allergyiap.test;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class TestIndex
@@ -13,27 +18,49 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestIndex")
 public class TestIndex extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestIndex() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public TestIndex() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String strStatus = "testando";
+		try {
+			InitialContext cxt = new InitialContext();
+			if (cxt != null) {
+				DataSource ds = (DataSource) cxt.lookup("java:/PostgresXADS");
+				if (ds == null)
+					strStatus = "Error while obtaining the dataSource";
+				else {
+					Connection connection = ds.getConnection();
+					Statement stm = connection.createStatement();
+					stm.executeUpdate("insert into example (code, name) values('123','456')");
+					connection.close();
+					stm.close();
+				}
+			}
+		} catch (Exception e) {
+			strStatus = e.getMessage();
+		}
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append(strStatus);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
