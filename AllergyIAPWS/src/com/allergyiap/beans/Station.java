f2a93f3b2f3f03fa6a0310d99e636b2676a23ce3
Meritxell Jordana Gavieiro <meritxelljoga10@gmail.com>
2016-12-24 15:45:16 +0100

    Changes in station: The name is now the primary key and not a serial ID

--- AllergyIAPWS/src/com/allergyiap/beans/Station.java
+++ AllergyIAPWS/src/com/allergyiap/beans/Station.java
@@ -4,31 +4,16 @@ public class Station implements java.io.Serializable{
 
 	private static final long serialVersionUID = 2L;
 	
-	private long id_station;
 	private String name_station;
 	private String latitude;
 	private String longitude;
 	
 	public Station(){}
 	
-	public Station(long id_station, String name_station, String latitude, String longitude){
-		this.id_station = id_station;
-		this.name_station = name_station;
-		this.latitude = latitude;
-		this.longitude = longitude;
-	}
-	
 	public Station(String name_station, String latitude, String longitude){
 		this.name_station = name_station;
 		this.latitude = latitude;
 		this.longitude = longitude;
-	}
-	
-	public void setId_station(long id_station){
-		this.id_station = id_station;
-	}
-	public long getId_station(){
-		return this.id_station;
 	}
 	
 	public void setName_station(String name_station){
